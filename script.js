7aa6c0331eb349642d3f0e427ee323ca42aac66c
chinrstyabclno <153152303+chinrstyabclno@users.noreply.github.com>
2024-10-06 17:26:56 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// When the "Offers" link is clicked, scroll to the Offers section
-document.querySelector('.nav-link[href="#offers"]').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default link behavior
-
-    // Scroll to the Offers section
-    document.querySelector('#offers').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
